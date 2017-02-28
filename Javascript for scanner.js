11070d2c79f430b722e963333cec1b571ac826cf
TheRealNinjauser <brandon.cameron.lee@gmail.com>
2017-02-28 12:33:21 +0000

    Added new and improved background ... and hopefully fixed the functionality of the scanner

--- Javascript for scanner.js
+++ Javascript for scanner.js
@@ -14,3 +14,17 @@ document.getElementById("go_button")
             var url = document.getElementById("url").value;
             runOCR(url);
         });
+
+$( '.gradient' ).mousemove(function( event ) {
+	var elementWidth =  $( this ).width() / 2,
+		elementHeight =  $( this ).height() / 2,
+		mouseX = event.pageX,
+		mouseY = event.pageY,
+		angle = Math.round( Math.atan2(mouseY - elementHeight, mouseX - elementWidth) * (180/Math.PI) ),
+		backGround = 'linear-gradient(' + angle + 'deg,#4ac1ff,#795bb0)';
+	
+	$( this ).css( 'background-image', backGround );
+});
+
+		
+		
